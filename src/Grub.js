54590d61f7faaf331b0a11ed26209aea57e5c496
jeremy lee <calijeremylee@gmail.com>
2018-04-30 15:31:49 -0700

    done

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,14 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
 };
+
+
+
+Grub(0, 'pink', 'jelly')
